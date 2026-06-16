f2480ebb44ca7fc0bafdf5b7a734ab0660f0abcb
m-suzuki <m-suzuki@pc2525.com>
2026-06-16 09:52:35 +0900

    test5

--- my-app/app/api/read-drive-pdf/route.ts
+++ my-app/app/api/read-drive-pdf/route.ts
@@ -2,10 +2,11 @@ export const runtime = "nodejs";
 import fs from "fs";
 import path from "path";
 import { NextResponse } from "next/server";
-// import { summarizePdf } from "../../../library/open_ai/summarizePdf";
-import {test} from "../../../library/test_open_ai/test";
+import { summarizePdf } from "../../../library/open_ai/summarizePdf";
+// import {test} from "../../../library/test_open_ai/test";
 import { createClient } from "@supabase/supabase-js";
 import { upload_supabase } from "../../../library/upload_supabase/upload_supabase";
+import { download_supabase } from "../../../library/download_supabase/download_supabase";
 import {logger} from "../../../library/logger/logger"
 import { notifySlack } from "../../../library/slack/notifyslack";
 
@@ -16,105 +17,40 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
-//supabaseにpdfを保存させる　原本をそのままで
-async function upload(pdfPath : string){
-
-  const fileBuffer = fs.readFileSync(pdfPath);
-
-  const fileName = path.basename(pdfPath);
-
-  const { data, error } = await supabase.storage
-    .from("origin_pdf_save")
-    .upload(`CustomerA${fileName}`, fileBuffer, {
-      contentType: "application/pdf",
-      upsert: true,
-    });
-
-  if (error) {
-    logger.error(error,"supabaseにデータが保存できませんでした")
-     }
-
-  // return data;
-
-};
-//supabaseにpdfを保存させる　原本をそのままで
 
 
-// //open aiにpdfファイルを読み込ませる
-// export async function GET() {
-//   try {
+//open aiにpdfファイルを読み込ませる
+export async function GET() {
+  try {
 
-//       const pdfPath = path.join(
-//       process.cwd(),
-//       "sample",
-//       "cp.pdf"
-//     );
+    //   const pdfPath = path.join(
+    //   process.cwd(),
+    //   "sample",
+    //   "cp.pdf"
+    // );
 
-//     if(!fs.existsSync(pdfPath)){
-//       throw new Error('該当PDFが存在しません: ${pdfPath}')
-//     }
+    // if(!fs.existsSync(pdfPath)){
+    //   throw new Error('該当PDFが存在しません: ${pdfPath}')
+    // }
+     
+    // originはsupbaseにあるpdfファイルの情報
+     const origin=await download_supabase()
 
-//      const summary = await summarizePdf(pdfPath);
+     return
 
-//     if(summary.error){
-//        return NextResponse.json(
-//       {
-//         error: String(summary.error),
-//       },
-//        );
-//     }
+     const summary = await summarizePdf(origin);
 
-//      await upload(pdfPath);
-//      await upload_supabase(summary);
+    if(summary.error){
+       return NextResponse.json(
+      {
+        error: String(summary.error),
+      },
+       );
+    }
 
-//     //  slackの通知だけ別のtry分に書く
-//     try{
-//      await notifySlack("PDFの要約処理が完了しました")
-//     }catch(slackError){
-//       logger.error(slackError,"slack通知に失敗しました")
-//     }
-//     // slack通知だけ別のtyr分に書く
+     await upload_supabase(summary);
 
-//     return NextResponse.json(
-//       {
-//         success: true, //これは現時点で使っていない
-//         summary: summary.ai_summary,
-//       });
-
-
-//   } catch (error) {
-    
-
-//     return NextResponse.json(
-//       {
-//         error: String(error),
-//       },
-//       { status: 500 }
-//     );
-//   }
-// }
-// //open aiにpdfファイルを読み込ませる
-
-
-
-
-
-
-
-// //open aiの代わりに挙動を見るためのプログラム
-export async function GET(){
-  try{
-
-      const pdfPath = path.join(
-      process.cwd(),
-      "sample",
-      "cp.pdf"
-    );
-    const summary=await test(); 
-    await upload(pdfPath);
-    await upload_supabase(summary);
-
-  // slackの通知だけ別のtry分に書く
+    //  slackの通知だけ別のtry分に書く
     try{
      await notifySlack("PDFの要約処理が完了しました")
     }catch(slackError){
@@ -124,10 +60,14 @@ export async function GET(){
 
     return NextResponse.json(
       {
-      summary: summary.ai_summary
-    });
+        success: true, //これは現時点で使っていない
+        summary: summary.ai_summary,
+      });
 
-  }catch (error) {
+
+  } catch (error) {
+    
+
     return NextResponse.json(
       {
         error: String(error),
@@ -136,4 +76,48 @@ export async function GET(){
     );
   }
 }
-//open aiの代わりに挙動を見るためのプログラム
+//open aiにpdfファイルを読み込ませる
+
+
+
+
+
+
+
+// // //open aiの代わりに挙動を見るためのプログラム
+// export async function GET(){
+//   try{
+
+//     //   const pdfPath = path.join(
+//     //   process.cwd(),
+//     //   "sample",
+//     //   "cp.pdf"
+//     // );
+
+//     const origin=await download_supabase()
+//     const summary=await test(); 
+//     await upload_supabase(summary);
+
+//   // slackの通知だけ別のtry分に書く
+//     try{
+//      await notifySlack("PDFの要約処理が完了しました")
+//     }catch(slackError){
+//       logger.error(slackError,"slack通知に失敗しました")
+//     }
+//     // slack通知だけ別のtyr分に書く
+
+//     return NextResponse.json(
+//       {
+//       summary: summary.ai_summary
+//     });
+
+//   }catch (error) {
+//     return NextResponse.json(
+//       {
+//         error: String(error),
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
+// //open aiの代わりに挙動を見るためのプログラム
--- my-app/library/download_supabase/download_supabase.ts
+++ my-app/library/download_supabase/download_supabase.ts
@@ -7,12 +7,26 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
-export async function GET() {
-  const filePath = "2026_6/monthly_report_xxx.pdf";
+export async function download_supabase() {
 
-  const { data, error } = await supabase.storage
-    .from("pdf")
-    .download(filePath);
+  const {data: files, error}=await supabase.storage
+  .from("pdf")
+  .list("2026_6");
+
+  console.log(files);
+
+  return
+
+//   const target = files?.find((item : {name:string}) =>
+//   /^monthly_report_.*\.pdf$/.test(item.name)
+// );
+
+const filePath = `2026_6/${target.name}`;
+
+const { data, error } = await supabase.storage
+  .from("pdf")
+  .download(filePath);
+
 
   if (error) {
     return new Response("PDF not found", { status: 404 });
@@ -21,7 +35,7 @@ export async function GET() {
   return new Response(data, {
     headers: {
       "Content-Type": "application/pdf",
-      "Content-Disposition": 'attachment; filename="cp.pdf"',
+      "Content-Disposition": `attachment; filename="${target.name}"`,
     },
   });
 }
