749e5d62d7e55aac4552be5f7a122a4acbbe02e9
m-suzuki <m-suzuki@pc2525.com>
2026-06-18 11:58:48 +0900

    test

--- my-app/app/mail_list/page.tsx
+++ my-app/app/mail_list/page.tsx
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { logger } from "../logger/logger";
+import { logger } from "../../library/logger/logger";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
