82a994b0eb31e11380218c17ac23438842e18025
m-suzuki <m-suzuki@pc2525.com>
2026-06-18 11:01:57 +0900

    test442 2

---
+++ my-app/app/mail_list/mail_list.tsx
@@ -0,0 +1,11 @@
+export default function EmailsPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>メール確認画面</h1>
+
+      <div>
+        ここにSupabaseから取得したメール一覧を表示
+      </div>
+    </main>
+  );
+}
