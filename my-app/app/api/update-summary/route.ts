7f3aae4d15f6393fa837d66cbedc1a502224ea50
m-suzuki <m-suzuki@rem2525.com>
2026-06-25 14:20:27 +0900

    aaaa

--- my-app/app/api/update-summary/route.ts
+++ my-app/app/api/update-summary/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-// a
+
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
